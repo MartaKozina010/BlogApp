805e6b8f43b325c10756ca0703665f469df0ab68
Marta Kozina <kozina.m@e80group.it>
2022-01-30 23:32:59 +0100

    feat: added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
 import './App.css';
-import { Button } from './components/Button';
-
-const StyledButton = styled(Button)`
-  margin: 5px;
-`
+import { Header } from "./components/Header/Header"
 
 function App() {
   return (
     <div className="App">
-      <StyledButton variant="darkBlue">View our work</StyledButton>
-      <Button variant='lightBlue' width="full">View our wor</Button>
-      <Button variant="darkYellow" width={350}>View our wor</Button>
-      <Button variant='lightYellow' width={150}>View our wor</Button>
+      <Header />
     </div>
   );
 }
